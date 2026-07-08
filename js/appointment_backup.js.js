ab882f9072caeb741f0971b6b8f2ca62b4e8c977
sudharshan <sudharshan05163@gmail.com>
2026-07-08 23:05:34 +0530

    message

---
+++ js/appointment_backup.js.js
@@ -0,0 +1,123 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const tableBody = document.querySelector(".appointments-table tbody");
+
+    let appointments = [];
+
+    async function loadAppointments() {
+
+    try {
+
+        console.log("Loading appointments...");
+
+        const response = await fetch(CONFIG.SCRIPT_URL + "?action=appointments");
+
+        const result = await response.json();
+
+        console.log("Backend Response:", result);
+
+        if (!result.success) {
+
+            console.log("Backend returned success = false");
+
+            return;
+
+        }
+
+        appointments = result.data;
+
+        console.log("Appointments Array:", appointments);
+
+        renderAppointments();
+
+    } catch (error) {
+
+        console.error("Load Error:", error);
+
+    }
+
+}
+
+    function renderAppointments() {
+
+    console.log("Table Body:", tableBody);
+
+    console.log("Appointments Count:", appointments.length);
+
+    tableBody.innerHTML = "";
+
+    appointments.forEach(app => {
+
+        console.log(app);
+
+        tableBody.innerHTML += `
+            <tr>
+                <td>${app.patientName}</td>
+                <td>${app.doctor}</td>
+                <td>${app.department}</td>
+                <td>${app.date}</td>
+<td>${app.time}</td>
+                <td>${app.status}</td>
+                <td>
+    <button class="edit-btn" data-id="${app.id}">Edit</button>
+    <button class="delete-btn" data-id="${app.id}">Delete</button>
+</td>
+            </tr>
+        `;
+
+    });
+
+}
+function formatDate(dateString) {
+
+    const date = new Date(dateString);
+
+    return date.toLocaleDateString("en-GB");
+
+}
+
+function formatTime(timeString) {
+
+    const date = new Date(timeString);
+
+    return date.toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true
+    });
+
+}
+
+document.addEventListener("click", (e) => {
+
+    try {
+
+        if (e.target.classList.contains("edit-btn")) {
+
+            const id = e.target.dataset.id;
+
+            console.log("Edit Clicked:", id);
+
+            console.log("Appointments variable:", appointments);
+
+            console.log("Type:", typeof appointments);
+
+            console.log("Length:", appointments.length);
+
+            const appointment = appointments.find(app => app.id === id);
+
+            console.log("Appointment Found:", appointment);
+
+        }
+
+    } catch (err) {
+
+        console.error("ERROR:", err);
+
+    }
+
+});
+
+    loadAppointments();
+
+});
